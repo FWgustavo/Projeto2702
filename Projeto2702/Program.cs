29ff93a805a48c10bdacb48eb00d12e43f5a8fac
Usuario <Usuario@CPD02-M01>
2024-02-27 10:36:47 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2702/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2702
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.Write("Digite o seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("O nome digitado foi: " + nome);
+
+            Console.ReadKey();
+        }
+    }
+}
